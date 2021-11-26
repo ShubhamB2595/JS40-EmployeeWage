56515b6a28cf9b3b5922f09bb9b27bbd5aca0b79
ShubhamB2595 <shubhambhele95@gmail.com>
2021-11-26 09:32:48 +0530

    UC8 solved, storing daily wage along with monthly wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -9,6 +9,8 @@ const MAX_HRS_IN_MONTH = 100;
 let isPresent = 1;
 let empHrs = 0, empWage = 0, monthlyWage = 0;
 let totalEmpHrs = 0, totalWorkingDays = 0;
+let empDailyWage = new Array();
+let day = 1;
 
 function getWorkingHours(empCheck) {
 	switch(empCheck) {
@@ -24,6 +26,10 @@ function getWorkingHours(empCheck) {
 	return empHrs;
 }
 
+function calculateDailyWage(empHrs) {
+    return empHrs * EMP_RATE_PER_HOUR;
+}
+
 while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
     	totalWorkingDays++;
 
@@ -31,7 +37,15 @@ while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS
 
 	empHrs = getWorkingHours(empCheck);
 	totalEmpHrs += empHrs;
+	empDailyWage.push(calculateDailyWage(empHrs));
 }
 
-monthlyWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+monthlyWage = calculateDailyWage(totalEmpHrs);
+
+console.log("Daily Wage History:");
+while(day < empDailyWage.length){
+    console.log("Day "+day+": "+ empDailyWage[day]);
+    day++;
+}
+
 console.log("Employee Monthly Wage = "+ monthlyWage);
