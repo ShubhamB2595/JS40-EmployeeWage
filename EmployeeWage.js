42b5c4cc8e00404016c87dac078daf1b6e0c669d
ShubhamB2595 <shubhambhele95@gmail.com>
2021-11-26 09:17:25 +0530

    UC6 solved, refactored using while condition

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,12 +3,15 @@ const EMP_RATE_PER_HOUR = 20;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 100;
 
 // Variables
 let isPresent = 1;
 let empHrs = 0, empWage = 0, monthlyWage = 0;
+let totalEmpHrs = 0, totalWorkingDays = 0;
 
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
+    	totalWorkingDays++;
 
 	let empCheck = Math.floor(Math.random() * 10) % 3;
 
@@ -22,11 +25,8 @@ for(let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
     	default:
         	empHrs = 0;
 	}
-
-	empWage = empHrs * EMP_RATE_PER_HOUR;
-	monthlyWage += empWage;
+	totalEmpHrs += empHrs;
 }
 
-
-
+monthlyWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 console.log("Employee Monthly Wage = "+ monthlyWage);
