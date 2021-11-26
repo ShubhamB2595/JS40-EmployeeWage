bf1d83f39f91611289b75fded01af5d7df2d78e7
ShubhamB2595 <shubhambhele95@gmail.com>
2021-11-26 09:04:46 +0530

    UC3 solved code added to get part time wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,13 +1,20 @@
 // Constants
 const EMP_RATE_PER_HOUR = 20;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
 
 // Variables
 let isPresent = 1;
-let empCheck = Math.floor(Math.random() * 10) % 2;
+let empCheck = Math.floor(Math.random() * 10) % 3;
 let empHrs = 0, empWage = 0;
-if (empCheck == 1)
+
+if (empCheck == IS_PART_TIME)
+    empHrs = 4;
+else if (empCheck == IS_FULL_TIME)
     empHrs = 8;
 else
     empHrs = 0;
+
 empWage = empHrs * EMP_RATE_PER_HOUR;
+
 console.log("Employee Wage = "+empWage);
