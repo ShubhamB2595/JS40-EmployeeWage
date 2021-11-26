88ae4f6f8cc3f41019b7b9473577116acadd92be
ShubhamB2595 <shubhambhele95@gmail.com>
2021-11-26 09:12:55 +0530

    UC5 solved, refactored code for calculating monthly wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,23 +2,31 @@
 const EMP_RATE_PER_HOUR = 20;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
+const NUM_OF_WORKING_DAYS = 20;
 
 // Variables
 let isPresent = 1;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empHrs = 0, empWage = 0;
+let empHrs = 0, empWage = 0, monthlyWage = 0;
 
-switch(empCheck) {
-    case IS_PART_TIME:
-        empHrs = 4;
-        break
-    case IS_FULL_TIME:
-        empHrs = 8;
-        break
-    default:
-        empHrs = 0;
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+
+	let empCheck = Math.floor(Math.random() * 10) % 3;
+
+	switch(empCheck) {
+   		case IS_PART_TIME:
+        	empHrs = 4;
+        	break
+    	case IS_FULL_TIME:
+        	empHrs = 8;
+        	break
+    	default:
+        	empHrs = 0;
+	}
+
+	empWage = empHrs * EMP_RATE_PER_HOUR;
+	monthlyWage += empWage;
 }
 
-empWage = empHrs * EMP_RATE_PER_HOUR;
 
-console.log("Employee Wage = "+empWage);
+
+console.log("Employee Monthly Wage = "+ monthlyWage);
